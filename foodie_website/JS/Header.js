e259d6e81839002bb951c1f5b2b06d90b9ee6660
dinesh-kandula <dinesh.kandula@techvedika.com>
2023-09-08 19:25:20 +0530

    Updated Responsive for mobile screen resolution (for less than 576px) for Foodie_website.

---
+++ foodie_website/JS/Header.js
@@ -0,0 +1,154 @@
+
+function onDisplayMenu(){
+    const navOptionsMdEl = document.getElementById("navOptionsMd");
+    navOptionsMdEl.classList.toggle("hide-nav-option-md");
+}
+
+// function onClickHeaderMenu(){
+//     const menuOptionsEl = document.getElementById("menuOptions");
+//     const menuDownArrowEl = document.getElementById("menuDownArrow");
+//     const menuUpArrowEl = document.getElementById("menuUpArrow");
+//     const menuDownArrowMdEl = document.getElementById("menuDownArrowMd");
+//     const menuUpArrowMdEl = document.getElementById("menuUpArrowMd");
+
+//     menuOptionsEl.classList.toggle("hide-menu-options");
+//     menuDownArrowEl.classList.toggle("hide-menu-options");
+//     menuUpArrowEl.classList.toggle("hide-menu-options");
+//     menuDownArrowMdEl.classList.toggle("hide-menu-options");
+//     menuUpArrowMdEl.classList.toggle("hide-menu-options");
+
+//     const servicesOptionsEl = document.getElementById("servicesOptions");
+//     const servicesDownArrowEl = document.getElementById("servicesDownArrow");
+//     const servicesUpArrowEl = document.getElementById("servicesUpArrow");
+//     const servicesDownArrowMdEl = document.getElementById("servicesDownArrowMd");
+//     const servicesUpArrowMdEl = document.getElementById("servicesUpArrowMd");
+
+//     servicesOptionsEl.classList.add("hide-menu-options");
+//     servicesDownArrowEl.classList.remove("hide-menu-options");
+//     servicesUpArrowEl.classList.add("hide-menu-options");
+//     servicesDownArrowMdEl.classList.remove("hide-menu-options");
+//     servicesUpArrowMdEl.classList.add("hide-menu-options");
+
+// }
+
+// function onClickServices(){
+//     const servicesOptionsEl = document.getElementById("servicesOptions");
+//     const servicesDownArrowEl = document.getElementById("servicesDownArrow");
+//     const servicesUpArrowEl = document.getElementById("servicesUpArrow");
+//     const servicesDownArrowMdEl = document.getElementById("servicesDownArrowMd");
+//     const servicesUpArrowMdEl = document.getElementById("servicesUpArrowMd");
+
+//     servicesOptionsEl.classList.toggle("hide-menu-options");
+//     servicesDownArrowEl.classList.toggle("hide-menu-options");
+//     servicesUpArrowEl.classList.toggle("hide-menu-options");
+//     servicesDownArrowMdEl.classList.toggle("hide-menu-options");
+//     servicesUpArrowMdEl.classList.toggle("hide-menu-options");
+
+//     const menuOptionsEl = document.getElementById("menuOptions");
+//     const menuDownArrowEl = document.getElementById("menuDownArrow");
+//     const menuUpArrowEl = document.getElementById("menuUpArrow");
+//     const menuDownArrowMdEl = document.getElementById("menuDownArrowMd");
+//     const menuUpArrowMdEl = document.getElementById("menuUpArrowMd");  
+
+//     menuOptionsEl.classList.add("hide-menu-options");
+//     menuDownArrowEl.classList.remove("hide-menu-options");
+//     menuUpArrowEl.classList.add("hide-menu-options");
+//     menuDownArrowMdEl.classList.remove("hide-menu-options");
+//     menuUpArrowMdEl.classList.add("hide-menu-options");
+
+// }
+
+
+// <!-- Header Menu Options Section Starts 
+// <div class="menu-options-card hide-menu-options" id="menuOptions">
+//     <ul class="menu-options" >
+//         <li>
+//             <p>All Time Favorites</p>  
+//             <i class="fa-solid fa-caret-right fa-xl" style="color: #39db4a;"></i>
+//         </li>
+//         <li>
+//             <p>Sandvich</p>           
+//             <i class="fa-solid fa-caret-right fa-xl" style="color: #39db4a;"></i>
+//         </li>
+//         <li>
+//             <p>Biriyani</p>            
+//             <i class="fa-solid fa-caret-right fa-xl" style="color: #39db4a;"></i>
+//         </li>
+//         <li>
+//             <p>Burgers</p>            
+//             <i class="fa-solid fa-caret-right fa-xl" style="color: #39db4a;"></i>
+//         </li>
+//         <li>
+//             <p>Chinnes</p>      
+//             <i class="fa-solid fa-caret-right fa-xl" style="color: #39db4a;"></i>
+//         </li>
+//         <li>
+//             <p>Desearts</p>     
+//             <i class="fa-solid fa-caret-right fa-xl" style="color: #39db4a;"></i>
+//         </li>
+//         <li>
+//             <p>Ice-creams</p>     
+//             <i class="fa-solid fa-caret-right fa-xl" style="color: #39db4a;"></i>
+//         </li>
+//         <li>
+//             <p>Main Courses</p>     
+//             <i class="fa-solid fa-caret-right fa-xl" style="color: #39db4a;"></i>
+//         </li>
+//         <li>
+//             <p>Salads</p>     
+//             <i class="fa-solid fa-caret-right fa-xl" style="color: #39db4a;"></i>
+//         </li>
+//         <li>
+//             <p>South India</p>     
+//             <i class="fa-solid fa-caret-right fa-xl" style="color: #39db4a;"></i>
+//         </li>
+//         <li>
+//             <p>Soups</p>     
+//             <i class="fa-solid fa-caret-right fa-xl" style="color: #39db4a;"></i>
+//         </li>
+//         <li>
+//             <p>Starters</p>     
+//             <i class="fa-solid fa-caret-right fa-xl" style="color: #39db4a;"></i>
+//         </li>
+//         <li>
+//             <p>Tea & Coffee</p>     
+//             <i class="fa-solid fa-caret-right fa-xl" style="color: #39db4a;"></i>
+//         </li>
+//     </ul>
+// </div>
+// Header Menu Options Section Ends 
+
+// Header Menu Options Section Starts 
+// <div class="services-options-card hide-menu-options" id="servicesOptions">
+//     <ul class="services-options" >
+//         <li>
+//             <p>Dine-in</p>  
+//             <i class="fa-solid fa-caret-right fa-xl" style="color: #39db4a;"></i>
+//         </li>
+//         <li>
+//             <p>Take Away</p>           
+//             <i class="fa-solid fa-caret-right fa-xl" style="color: #39db4a;"></i>
+//         </li>
+//         <li>
+//             <p>Online Orders</p>            
+//             <i class="fa-solid fa-caret-right fa-xl" style="color: #39db4a;"></i>
+//         </li>
+//         <li>
+//             <p>Home Delivery</p>            
+//             <i class="fa-solid fa-caret-right fa-xl" style="color: #39db4a;"></i>
+//         </li>
+//         <li>
+//             <p>Cateering</p>      
+//             <i class="fa-solid fa-caret-right fa-xl" style="color: #39db4a;"></i>
+//         </li>
+//         <li>
+//             <p>Events</p>     
+//             <i class="fa-solid fa-caret-right fa-xl" style="color: #39db4a;"></i>
+//         </li>
+//         <li>
+//             <p>Banquet Halls</p>     
+//             <i class="fa-solid fa-caret-right fa-xl" style="color: #39db4a;"></i>
+//         </li>
+//     </ul>
+// </div>
+// Header Menu Options Section Ends  -->
